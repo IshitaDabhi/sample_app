cf713bc72b427db73edd70afa56c93e984172af2
isheeta.dabhi-BTC <isheeta.dabhi@beaverlogic.com>
2014-07-03 10:28:44 +0530

    Listing all users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,6 +7,15 @@ module SessionsHelper
 		self.current_user = user
 	end
 
+	def redirect_back_or(default)
+		redirect_to(session[:return_to] || default)
+		session.delete(:return_to)
+	end
+
+	def store_location
+		session[:return_to] = request.url if request.get?
+	end
+
 	def sign_out
 		current_user.update_attribute(:remember_token,User.digest(User.new_remember_token))
 		cookies.delete(:remember_token)
@@ -15,6 +24,11 @@ module SessionsHelper
 
 	def current_user=(user)
 		@current_user = user
+
+	end
+
+	def current_user?(user)
+		user ==	current_user	
 	end
 
 	def current_user
