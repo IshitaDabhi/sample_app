b70640d10815652e2499a9a8b0b332c7be35c052
isheeta.dabhi-BTC <isheeta.dabhi@beaverlogic.com>
2014-07-04 16:47:46 +0530

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,16 +1,5 @@
 require 'spec_helper'
 
-RSpec.describe "StaticPages", :type => :request do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-     #get static_pages_home_path
-      #expect(response.status).to be(200)
-    end
-  end
-end
-
-require 'spec_helper'
-
 describe "Static pages" do
 
 
@@ -25,11 +14,11 @@ subject { page }
     it { should_not have_title('| Home') }
 
 
-    describe "for signed-in users" do
+      describe "for signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        FactoryGirl.create(:micropost, user: user, content: "Lorem")
+        FactoryGirl.create(:micropost, user: user, content: "Ipsum")
         sign_in user
         visit root_path
       end
@@ -38,6 +27,17 @@ subject { page }
         user.feed.each do |item|
           expect(page).to have_selector("li##{item.id}", text: item.content)
         end
+      end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
       end
     end
   end
